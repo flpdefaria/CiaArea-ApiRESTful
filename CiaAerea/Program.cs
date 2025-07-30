60e09563fa4235f322bbc5fbe76a8aa7082b6078
Felipe Faria <afariaflp@gmail.com>
2025-07-30 20:52:54 -0300

    Create AeronaveService

--- CiaAerea/Program.cs
+++ CiaAerea/Program.cs
@@ -1,3 +1,5 @@
+using CiaAerea.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CiaAereaContext>();
 
 var app = builder.Build();
 
---
+++ CiaAerea/Services/AeronaveService.cs
@@ -0,0 +1,32 @@
+using CiaAerea.Contexts;
+using CiaAerea.Entities;
+using CiaAerea.ViewModels.Aeronave;
+
+namespace CiaAerea.Services;
+
+public class AeronaveService
+{
+    private readonly CiaAereaContext _context;
+
+    public AeronaveService(CiaAereaContext context)
+    {
+        _context = context;
+    }
+
+    public DetalhesAeronaveViewModel AdicionarAeronave(AdicionarAeronaveViewModel dados)
+    {
+        var aeronave = new Aeronave(dados.Fabricante, dados.Modelo, dados.Codigo);
+        
+        _context.Aeronaves.Add(aeronave);
+        _context.SaveChanges();
+        
+        return new DetalhesAeronaveViewModel
+        (
+            aeronave.Id, 
+            aeronave.Fabricante, 
+            aeronave.Modelo, 
+            aeronave.Codigo
+        );
+    }
+    
+}
