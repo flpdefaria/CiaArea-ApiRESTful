803121b408df08fddcc6700a3e56980d950e3794
Felipe Faria <afariaflp@gmail.com>
2025-07-29 21:41:08 -0300

    Add OnModelCreating in CiaAereaContext

--- CiaArea/Contexts/CiaAereaContext.cs
+++ CiaArea/Contexts/CiaAereaContext.cs
@@ -1,4 +1,5 @@
 using CiaArea.Entities;
+using CiaArea.EntityConfigurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace CiaArea.Contexts;
@@ -20,5 +21,13 @@ public class CiaAereaContext: DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer(_configuration.GetConnectionString("CiaAerea"));
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new AeronaveConfiguration());
+        modelBuilder.ApplyConfiguration(new PilotoConfiguration());
+        modelBuilder.ApplyConfiguration(new VooConfiguration());
+        modelBuilder.ApplyConfiguration(new CancelamentoConfiguration());
+        modelBuilder.ApplyConfiguration(new ManutencaoConfiguration());
     }
 }
