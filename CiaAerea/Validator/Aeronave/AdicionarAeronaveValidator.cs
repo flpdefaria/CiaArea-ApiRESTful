aab0afba84dec5be32a1933fdfb4dc9a12659589
Felipe Faria <afariaflp@gmail.com>
2025-08-11 22:20:10 -0300

    Create adicionarAeronaveValidator

---
+++ CiaAerea/Validator/Aeronave/AdicionarAeronaveValidator.cs
@@ -0,0 +1,29 @@
+using CiaAerea.Contexts;
+using CiaAerea.ViewModels.Aeronave;
+using FluentValidation;
+
+namespace CiaAerea.Validator.Aeronave;
+
+public class AdicionarAeronaveValidator:AbstractValidator<AdicionarAeronaveViewModel>
+{
+    private readonly CiaAereaContext _context;
+    
+    public AdicionarAeronaveValidator(CiaAereaContext context)
+    {
+        _context = context;
+        
+        RuleFor(a => a.Fabricante)
+            .NotEmpty().WithMessage("O campo Fabricante é obrigatório")
+            .MaximumLength(50).WithMessage("O campo Fabricante deve ter no máximo 50 caracteres");
+        
+        RuleFor(a => a.Modelo)
+            .NotEmpty().WithMessage("O campo Modelo é obrigatório")
+            .MaximumLength(50).WithMessage("O campo Modelo deve ter no máximo 50 caracteres");
+        
+        RuleFor(a => a.Codigo)
+            .NotEmpty().WithMessage("O campo Codigo é obrigatório")
+            .MaximumLength(10).WithMessage("O campo Codigo deve ter no máximo 10 caracteres")
+            .Must(codigo => !_context.Aeronaves.Any(a => a.Codigo == codigo)).WithMessage("Já existe uma aeronave com esse código");
+
+    }
+}
