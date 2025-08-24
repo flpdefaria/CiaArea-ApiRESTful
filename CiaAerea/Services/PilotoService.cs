56fbe23781f11e064de709ac2057701e8c521cd5
Felipe Faria <afariaflp@gmail.com>
2025-08-23 21:58:19 -0300

    Create pilotoservice

---
+++ CiaAerea/Services/PilotoService.cs
@@ -0,0 +1,32 @@
+using CiaAerea.Contexts;
+using CiaAerea.Entities;
+using CiaAerea.Validator.Piloto;
+using CiaAerea.ViewModels.Piloto;
+using FluentValidation;
+
+namespace CiaAerea.Services;
+
+public class PilotoService
+{
+    private readonly CiaAereaContext _context;
+
+    private readonly AdicionarPilotoValidator _adicionarPilotoValidator;
+
+    public PilotoService(CiaAereaContext context, AdicionarPilotoValidator adicionarPilotoValidator)
+    {
+        _context = context;
+        _adicionarPilotoValidator = adicionarPilotoValidator;
+    }
+
+    public DetalhesPilotoViewModel AdicionarPiloto(AdicionarPilotoViewModel dados)
+    {
+        _adicionarPilotoValidator.ValidateAndThrow(dados);
+        
+        var piloto = new Piloto(dados.Nome, dados.Matricula);
+        
+        _context.Add(piloto);
+        _context.SaveChanges();
+        
+        return new DetalhesPilotoViewModel(piloto.Id, piloto.Nome, piloto.Matricula);
+    }
+}
--- CiaAerea/Validator/Piloto/AdicionarPilotoValidator.cs
+++ CiaAerea/Validator/Piloto/AdicionarPilotoValidator.cs
@@ -14,11 +14,11 @@ public class AdicionarPilotoValidator: AbstractValidator<AdicionarPilotoViewMode
         
         RuleFor(p => p.Nome)
             .NotEmpty().WithMessage("O campo Nome é obrigatório")
-            .MaximumLength(50).WithMessage("O campo Nome deve ter no máximo 50 caracteres");
+            .MaximumLength(100).WithMessage("O campo Nome deve ter no máximo 100 caracteres");
         
         RuleFor(p => p.Matricula)
-            .NotEmpty().WithMessage("O campo Matricula é obrigatório")
-            .MaximumLength(10).WithMessage("O campo Matricula deve ter no máximo 10 caracteres")
-            .Must(matricula => !_context.Pilotos.Any(p => p.Matricula == matricula)).WithMessage("Já existe um piloto com essa matrícula");
+            .NotEmpty().WithMessage("O campo Matrícula é obrigatório")
+            .MaximumLength(10).WithMessage("O campo Matrícula deve ter no máximo 10 caracteres")
+            .Must(matricula => _context.Pilotos.Count(p => p.Matricula == matricula) == 0).WithMessage("Já existe um piloto com essa matrícula");
     }
 }
